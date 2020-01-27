b544ef103e8f27bc3b96490acfb6935af4858bdb
Kozo Nishida <kozo.nishida@gmail.com>
2020-01-28 07:18:46 +0900

    Update create3Dnetfile.R

--- create3Dnetfile.R
+++ create3Dnetfile.R
@@ -1,30 +1,58 @@
 library(RCy3)
 library(dplyr)
+library(KEGGREST)
 
 installApp('KEGGscape')
 
-create3Dnetfile <- function(kgml1, kgml2, kgml3, zheight1, zheight2, zheight3) {
-  importNetworkFromFile(file = kgml1) 
-  importNetworkFromFile(file = kgml2)
-  importNetworkFromFile(file = kgml3)
+create3Dnetfile <- function(pathwayID1, pathwayID2, pathwayID3, zheight1, zheight2, zheight3) {
+  writeLines(keggGet(pathwayID1, option = 'kgml'), paste(pathwayID1, '.xml', sep=''))
+  writeLines(keggGet(pathwayID2, option = 'kgml'), paste(pathwayID2, '.xml', sep=''))
+  writeLines(keggGet(pathwayID3, option = 'kgml'), paste(pathwayID3, '.xml', sep=''))
   
-  nodetable1 = getTableColumns('node', network = getNetworkList()[1])
+  importNetworkFromFile(file = paste(pathwayID1, '.xml', sep='')) 
+  importNetworkFromFile(file = paste(pathwayID2, '.xml', sep=''))
+  importNetworkFromFile(file = paste(pathwayID3, '.xml', sep=''))
+  
+  netlist = getNetworkList()
+  
+  print(paste("Reading", pathwayID1))
+  if (length(grep(pathwayID1, netlist)) > 1) {
+    net1index = grep(pathwayID1, netlist)[1]
+    nodetable1 = getTableColumns('node', network = netlist[net1index])
+    netlist = netlist[-net1index]
+  } else {
+    nodetable1 = getTableColumns('node', network = netlist[grep(pathwayID1, netlist)[1]])
+  }
   KEGG_NODE_Z = rep(zheight1, dim(nodetable1)[1])
   nodetable1 = cbind(nodetable1, KEGG_NODE_Z)
   
-  nodetable2 = getTableColumns('node', network = getNetworkList()[2])
+  print(paste("Reading", pathwayID2))
+  if (length(grep(pathwayID2, netlist)) > 1) {
+    net2index = grep(pathwayID2, netlist)[1]
+    nodetable2 = getTableColumns('node', network = netlist[net2index])
+    netlist = netlist[-net2index]
+  } else {
+    nodetable2 = getTableColumns('node', network = netlist[grep(pathwayID2, netlist)[1]])
+  }
   KEGG_NODE_Z = rep(zheight2, dim(nodetable2)[1])
   nodetable2 = cbind(nodetable2, KEGG_NODE_Z)  
   
-  nodetable3 = getTableColumns('node', network = getNetworkList()[3])
+  print(paste("Reading", pathwayID3))
+  nodetable3 = getTableColumns('node', network = netlist[grep(pathwayID3, netlist)[1]])
   KEGG_NODE_Z = rep(zheight3, dim(nodetable3)[1])
   nodetable3 = cbind(nodetable3, KEGG_NODE_Z)
   
+  print("Combining networks...")
   nodetable3d = bind_rows(nodetable1, nodetable2, nodetable3)
   nodetable3d %>% rename(id=SUID) -> nodes
   nodes['id'] = as.character(nodes$id)
   createNetworkFromDataFrames(nodes)
+  download.file("https://raw.githubusercontent.com/ecell/cytoscape-styles/master/xml/transomics.xml", "transomics.xml")
+  importVisualStyles(filename = "transomics.xml")
+  setVisualStyle("transomics")
+  print("Set visual style to 'transomics'")
   exportNetwork('./transomics','cyjs')
+  print("Wrote 'transomics.cyjs' in the current working directory. Please import it from Cytoscape Desktop and select 'Cy3D' from 'Network View Renderer' dropdown list. Then you should see the multi layered 3D network space.")
 }
 
 ## example
