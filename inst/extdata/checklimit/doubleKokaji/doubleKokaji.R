7c1ca58ac967246a63bc4b99d9dd8d557a07cb90
Kozo Nishida <kozo.nishida@gmail.com>
2023-09-25 02:53:47 +0900

    Update doubleKokaji.R

--- inst/extdata/checklimit/doubleKokaji/doubleKokaji.R
+++ inst/extdata/checklimit/doubleKokaji/doubleKokaji.R
@@ -1,7 +1,9 @@
+library(transomics2cytoscape)
+
 start.time <- Sys.time()
 networkDataDir <- tempfile(); dir.create(networkDataDir)
 tfs <- system.file("extdata/usecase2", "TFs.sif", package = "transomics2cytoscape")
-networkLayers <- "doubleKokaji.tsv"
+networkLayers <- "double_kokaji2020.tsv"
 stylexml <- system.file("extdata/usecase2", "Kokaji2020styles.xml", package = "transomics2cytoscape")
 suid <- create3Dnetwork(networkDataDir, networkLayers, stylexml)
 end.time <- Sys.time()
