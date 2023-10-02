290e42d1a76488b7a8846bb244f58ce9a9e645f6
Kozo Nishida <kozo.nishida@gmail.com>
2023-10-03 00:18:45 +0900

    Create fivefoldYugi.R

---
+++ inst/extdata/checklimit/fivefoldYugi/fivefoldYugi.R
@@ -0,0 +1,19 @@
+library(transomics2cytoscape)
+
+start.time <- Sys.time()
+
+networkDataDir <- tempfile(); dir.create(networkDataDir)
+networkLayers <- "fivefold_yugi2014.tsv"
+file.copy(networkLayers, networkDataDir)
+stylexml <- system.file("extdata/usecase1", "yugi2014.xml", package = "transomics2cytoscape")
+suid <- create3Dnetwork(networkDataDir, networkLayers, stylexml)
+layer1to2 <- "k2e_fivefold.tsv"
+layer3to2 <- "fivefold_allosteric_ec2rea.tsv"
+file.copy(layer1to2, networkDataDir)
+file.copy(layer3to2, networkDataDir)
+suid <- createTransomicEdges(suid, layer1to2)
+suid <- createTransomicEdges(suid, layer3to2)
+
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+print(time.taken)
